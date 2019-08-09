3f15aba6ed314925451a57fe0bf86d00c6e72567
DMeppiel <daniel.meppiel@wegaw.com>
2019-08-09 16:53:48 +0200

    Changed to username/password example

--- tests/defrost_test.sh
+++ tests/defrost_test.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
+export USERNAME="YOUR USERNAME HERE"
+export PASSWORD="YOUR PASSWORD HERE"
 # URLs used in this example
-export DEFROST_API_URL="http://api.dev.defrost.ch/v1/snow-covers/"
+export DEFROST_TOKEN_URL="http://api.dev.defrost.ch/v1/token/"
 export DEFROST_REFRESH_URL="http://api.dev.defrost.ch/v1/token/refresh/"
-# your API tokens
-export DEFROST_JWT_ACCESS_TOKEN="YOUR ACCESS TOKEN GOES HERE"
-export DEFROST_JWT_REFRESH_TOKEN="YOUR REFRESH TOKEN GOES HERE"
+export DEFROST_SNOWMAP_URL="http://api.dev.defrost.ch/v1/snow-covers/"
 
-# if necessary, this is how you would refresh your access token
+# To obtain your set of tokens: First we execute the request with curl
+# And then we parse it with python 3. You can also get your token from
+# DeFROST dashboard.
+DEFROST_JWT_ACCESS_TOKEN=`curl -H "Content-Type: application/json" -X POST -d '{"username": "'$USERNAME'", "password":"'$PASSWORD'"}' "$DEFROST_TOKEN_URL" | \
+    python3 -c "import sys, json; print(json.load(sys.stdin)['access'])"`
+echo "ACCESS TOKEN: $DEFROST_JWT_ACCESS_TOKEN"
+# For readability, we extract the refresh token repeating the previous request
+DEFROST_JWT_REFRESH_TOKEN=`curl -H "Content-Type: application/json" -X POST -d '{"username": "'$USERNAME'", "password":"'$PASSWORD'"}' "$DEFROST_TOKEN_URL" | \
+    python3 -c "import sys, json; print(json.load(sys.stdin)['refresh'])"`
+echo "REFRESHTOKEN: $DEFROST_JWT_REFRESH_TOKEN"
+# If necessary, this is how you would refresh your access token
 DEFROST_JWT_ACCESS_TOKEN=`curl -X POST -F "refresh=$DEFROST_JWT_REFRESH_TOKEN" "$DEFROST_REFRESH_URL" | sed -E "s/\{\"access\":\"(.+?)\"\}/\1/gI"`
 echo "Your new access token is $DEFROST_JWT_ACCESS_TOKEN"
-# you can now make an API call
+# With your access token, you can make API calls
 # make sure you always include your access token in every request
-DATA=`curl --header "Authorization: Bearer $DEFROST_JWT_ACCESS_TOKEN" "$DEFROST_API_URL"`
+DATA=`curl --header "Authorization: Bearer $DEFROST_JWT_ACCESS_TOKEN" "$DEFROST_SNOWMAP_URL"`
 echo "DATA RECEIVED: $DATA"
